7a1e92afa8ecb7002a2e296f1c628dedb2f963ac
MK1104 <s3978862@rmit.edu.vn>
2025-09-22 11:27:13 +0700

    Create Player Class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,24 @@
+import Player.PlayerManagement;
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n=== Pickleball System ===");
+            System.out.println("1. Create Player");
+            System.out.println("2. List Players");
+            System.out.println("3. Exit");
+            System.out.print("Choose option: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1 -> PlayerManagement.savePlayers();
+                case 2 -> PlayerManagement.listPlayers();
+                case 3 -> { System.out.println("Goodbye!"); return; }
+                default -> System.out.println("Invalid choice.");
+            }
+        }
+    }
 }
